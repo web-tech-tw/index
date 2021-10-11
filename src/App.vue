263e93eb91b6ccedecac93a92e1e385c64c649f9
SuperSonic <supersonic@livemail.tw>
2021-10-12 01:41:19 +0800

    Update

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,31 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+    <v-app-bar app color="primary" dark>
+      <v-app-bar-title>Taiwan Web Technology Promote</v-app-bar-title>
     </v-app-bar>
-
     <v-main>
-      <HelloWorld/>
+      <v-container>
+        <v-card>
+          <v-card-title>
+            Taiwan Web Technology Promote
+          </v-card-title>
+          <v-card-subtitle>
+            臺灣網際網路技術推廣組織
+          </v-card-subtitle>
+          <v-card-actions>
+            <v-spacer/>
+            <v-btn link href="https://discord.gg/xpyuq342nX" class="purple white--text">
+              Discord
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
   name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
 };
 </script>
