37c93cc0ca027b96212acc5f6ab75060ce11aa69
SuperSonic <supersonic@livemail.tw>
2021-12-13 23:37:24 +0800

    Update

---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="bg-gray-50">
+    <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
+      <h2 class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+        <span class="block">Taiwan Web Technology Promotion Organization</span>
+        <span class="block text-indigo-600">臺灣網際網路技術推廣組織</span>
+      </h2>
+      <div class="mt-8 flex lg:mt-0 lg:flex-shrink-0">
+        <div class="inline-flex rounded-md shadow">
+          <a
+              href="https://discord.gg/xpyuq342nX"
+              class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
+          >
+            Discord
+          </a>
+        </div>
+        <div class="ml-3 inline-flex rounded-md shadow">
+          <a
+              href="https://github.com/web-tech-tw"
+              class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-indigo-50"
+          >
+            GitHub
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
