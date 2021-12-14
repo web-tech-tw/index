899bde8dd3066c9d8796f0411d0e2a61956624bf
SuperSonic <supersonic@livemail.tw>
2021-12-14 16:54:36 +0800

    Update

--- src/App.vue
+++ src/App.vue
@@ -77,16 +77,15 @@
                 </div>
               </div>
             </div>
-            <div v-else-if="item.type === 'function'">
-              <button
-                  aria-expanded="false"
-                  class="text-base font-medium text-gray-500 hover:text-gray-900"
-                  type="button"
-                  @click="item.action"
-              >
-                {{ item.name }}
-              </button>
-            </div>
+            <button
+                v-else-if="item.type === 'function'"
+                aria-expanded="false"
+                class="text-base font-medium text-gray-500 hover:text-gray-900"
+                type="button"
+                @click="item.action"
+            >
+              {{ item.name }}
+            </button>
           </div>
         </nav>
         <div class="-mr-2 -my-2 md:hidden">
@@ -105,10 +104,11 @@
         </div>
       </div>
     </div>
-    <div v-show="mobile_menu.status" class="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
+    <div v-show="mobile_menu.status"
+         class="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
       <div class="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
         <div class="pt-5 pb-6 px-5">
-          <div @click="mobile_menu.status = false" class="flex items-center justify-between">
+          <div class="flex items-center justify-between" @click="mobile_menu.status = false">
             <div>
               <h1 class="flex-auto text-lg font-semibold text-gray-900 sm:hidden">
                 Web Tech TW
@@ -128,20 +128,86 @@
           </div>
           <div class="mt-6">
             <nav class="grid gap-y-8">
-              <button
-                  v-for="(item, index) in menu" :key="index"
-                  class="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50"
-                  type="button"
-              >
-                <svg aria-hidden="true" class="flex-shrink-0 h-6 w-6 text-indigo-600" fill="none"
-                     stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                  <path
-                      d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
-                      stroke-linecap="round" stroke-linejoin="round"
-                      stroke-width="2"/>
-                </svg>
-                <span class="ml-3 text-base font-medium text-gray-900">{{ item.name }}</span>
-              </button>
+              <div v-for="(item, index) in menu" :key="index">
+                <div v-if="item.type === 'dropdown'" class="relative">
+                  <button
+                      aria-expanded="false"
+                      class="text-gray-500 group bg-white rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 w-full"
+                      type="button"
+                      @click="item.status = !item.status"
+                  >
+                    <span>{{ item.name }}</span>
+                    <svg
+                        aria-hidden="true"
+                        class="text-gray-400 ml-2 h-5 w-5 group-hover:text-gray-500 ml-auto"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                          clip-rule="evenodd"
+                          d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                          fill-rule="evenodd"
+                      />
+                    </svg>
+                  </button>
+                  <div
+                      class="absolute z-10 -ml-4 mt-3 transform px-2 w-screen max-w-md sm:px-0 lg:ml-0 lg:left-1/2 lg:-translate-x-1/2">
+                    <div
+                        v-show="item.status"
+                        class="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden"
+                    >
+                      <div class="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
+                        <div
+                            v-for="(child, index) in item.items"
+                            :key="index"
+                            class="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50 cursor-pointer"
+                            @click="child.action"
+                        >
+                          <svg
+                              aria-hidden="true"
+                              class="flex-shrink-0 h-6 w-6 text-indigo-600"
+                              fill="none"
+                              stroke="currentColor"
+                              viewBox="0 0 24 24"
+                              xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path
+                                d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                            />
+                          </svg>
+                          <div class="ml-4">
+                            <p class="text-base font-medium text-gray-900">
+                              {{ child.name }}
+                            </p>
+                            <p class="mt-1 text-sm text-gray-500">
+                              {{ child.description }}
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <button
+                    v-else-if="item.type === 'function'"
+                    class="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50 w-full"
+                    type="button"
+                    @click="item.action"
+                >
+                  <svg aria-hidden="true" class="flex-shrink-0 h-6 w-6 text-indigo-600" fill="none"
+                       stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
+                        stroke-linecap="round" stroke-linejoin="round"
+                        stroke-width="2"/>
+                  </svg>
+                  <span class="ml-3 text-base font-medium text-gray-900">{{ item.name }}</span>
+                </button>
+              </div>
             </nav>
           </div>
         </div>
