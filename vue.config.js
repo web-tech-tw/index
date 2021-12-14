42e87272135c25c0b3eb46ee96fa5434cf6fef00
SuperSonic <supersonic@livemail.tw>
2021-12-14 18:00:13 +0800

    Add vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    pages: {
+        index: {
+            title: 'Taiwan Web Technology Promotion Organization 臺灣網際網路技術推廣組織',
+            entry: 'src/main.js',
+            template: 'public/index.html',
+            filename: 'index.html',
+        }
+    },
+};
