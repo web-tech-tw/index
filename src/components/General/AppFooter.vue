240b3045cde624d3bbd084c886cd78a1e4ed1b42
SuperSonic <supersonic@livemail.tw>
2021-12-16 03:19:11 +0800

    Preparing

---
+++ src/components/General/AppFooter.vue
@@ -0,0 +1,76 @@
+<template>
+  <footer class="mt-16 pb-16 text-sm leading-6">
+    <div class="max-w-7xl mx-auto divide-y divide-gray-200 px-4 sm:px-6 md:px-8">
+      <div class="flex">
+        <div class="flex-none w-1/2 space-y-10 sm:space-y-8 lg:flex lg:space-y-0">
+          <div class="lg:flex-none lg:w-1/2">
+            <h2 class="font-semibold text-gray-900">Getting Started</h2>
+            <ul class="mt-3 space-y-2">
+              <li><a class="hover:text-gray-900" href="/docs/installation">Installation</a></li>
+              <li><a class="hover:text-gray-900" href="/docs/editor-setup">Editor Setup</a></li>
+              <li><a class="hover:text-gray-900" href="/docs/using-with-preprocessors">Using with
+                Preprocessors</a></li>
+              <li><a class="hover:text-gray-900" href="/docs/optimizing-for-production">Optimizing for
+                Production</a></li>
+              <li><a class="hover:text-gray-900" href="/docs/browser-support">Browser Support</a></li>
+              <li><a class="hover:text-gray-900" href="/docs/upgrade-guide">Upgrade Guide</a></li>
+            </ul>
+          </div>
+          <div class="lg:flex-none lg:w-1/2">
+            <h2 class="font-semibold text-gray-900">Core Concepts</h2>
+            <ul class="mt-3 space-y-2">
+              <li><a class="hover:text-gray-900" href="/docs/utility-first">Utility-First
+                Fundamentals</a></li>
+              <li><a class="hover:text-gray-900" href="/docs/hover-focus-and-other-states">Handling
+                Hover, Focus, and Other States</a></li>
+              <li><a class="hover:text-gray-900" href="/docs/responsive-design">Responsive Design</a>
+              </li>
+              <li><a class="hover:text-gray-900" href="/docs/dark-mode">Dark Mode</a></li>
+              <li><a class="hover:text-gray-900" href="/docs/reusing-styles">Reusing Styles</a></li>
+              <li><a class="hover:text-gray-900" href="/docs/adding-custom-styles">Adding Custom
+                Styles</a></li>
+              <li><a class="hover:text-gray-900" href="/docs/functions-and-directives">Functions &amp;
+                Directives</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="flex-none w-1/2 space-y-10 sm:space-y-8 lg:flex lg:space-y-0">
+          <div class="lg:flex-none lg:w-1/2">
+            <h2 class="font-semibold text-gray-900">Customization</h2>
+            <ul class="mt-3 space-y-2">
+              <li><a class="hover:text-gray-900" href="/docs/configuration">Configuration</a></li>
+              <li><a class="hover:text-gray-900" href="/docs/content-configuration">Content
+                Configuration</a></li>
+              <li><a class="hover:text-gray-900" href="/docs/theme">Theme Configuration</a></li>
+              <li><a class="hover:text-gray-900" href="/docs/screens">Customizing Screens</a></li>
+              <li><a class="hover:text-gray-900" href="/docs/customizing-colors">Customizing
+                Colors</a></li>
+              <li><a class="hover:text-gray-900" href="/docs/customizing-spacing">Customizing
+                Spacing</a></li>
+              <li><a class="hover:text-gray-900" href="/docs/plugins">Plugins</a></li>
+              <li><a class="hover:text-gray-900" href="/docs/presets">Presets</a></li>
+            </ul>
+          </div>
+          <div class="lg:flex-none lg:w-1/2">
+            <h2 class="font-semibold text-gray-900">Community</h2>
+            <ul class="mt-3 space-y-2">
+              <li><a class="hover:text-gray-900"
+                     href="https://github.com/tailwindlabs/tailwindcss">GitHub</a></li>
+              <li><a class="hover:text-gray-900" href="/discord">Discord</a></li>
+              <li><a class="hover:text-gray-900" href="https://twitter.com/tailwindcss">Twitter</a>
+              </li>
+              <li><a class="hover:text-gray-900"
+                     href="https://www.youtube.com/tailwindlabs">YouTube</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'AppFooter'
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,112 @@
 <template>
-  <div class="bg-gray-50">
-    <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
-      <h2 class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-        <span class="block mb-3">Taiwan Web Technology Promotion Organization</span>
-        <span class="block text-indigo-600">臺灣網際網路技術推廣組織</span>
-      </h2>
+  <div>
+    <div class="bg-gray-50">
+      <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
+        <h2 class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+          <span class="block mb-3">Taiwan Web Technology Promotion Organization</span>
+          <span class="block text-indigo-600">臺灣網際網路技術推廣組織</span>
+        </h2>
+      </div>
+    </div>
+    <div class="py-12 bg-white">
+      <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="lg:text-center">
+          <h2 class="text-base text-indigo-600 font-semibold tracking-wide uppercase">Transactions</h2>
+          <p class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+            A better way to send money
+          </p>
+          <p class="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+            Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam voluptatum cupiditate veritatis in accusamus quisquam.
+          </p>
+        </div>
+
+        <div class="mt-10">
+          <dl class="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
+            <div class="relative">
+              <dt>
+                <div class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                  <!-- Heroicon name: outline/globe-alt -->
+                  <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
+                  </svg>
+                </div>
+                <p class="ml-16 text-lg leading-6 font-medium text-gray-900">Competitive exchange rates</p>
+              </dt>
+              <dd class="mt-2 ml-16 text-base text-gray-500">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
+              </dd>
+            </div>
+
+            <div class="relative">
+              <dt>
+                <div class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                  <!-- Heroicon name: outline/scale -->
+                  <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3" />
+                  </svg>
+                </div>
+                <p class="ml-16 text-lg leading-6 font-medium text-gray-900">No hidden fees</p>
+              </dt>
+              <dd class="mt-2 ml-16 text-base text-gray-500">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
+              </dd>
+            </div>
+
+            <div class="relative">
+              <dt>
+                <div class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                  <!-- Heroicon name: outline/lightning-bolt -->
+                  <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z" />
+                  </svg>
+                </div>
+                <p class="ml-16 text-lg leading-6 font-medium text-gray-900">Transfers are instant</p>
+              </dt>
+              <dd class="mt-2 ml-16 text-base text-gray-500">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
+              </dd>
+            </div>
+
+            <div class="relative">
+              <dt>
+                <div class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                  <!-- Heroicon name: outline/annotation -->
+                  <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
+                  </svg>
+                </div>
+                <p class="ml-16 text-lg leading-6 font-medium text-gray-900">Mobile notifications</p>
+              </dt>
+              <dd class="mt-2 ml-16 text-base text-gray-500">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
+              </dd>
+            </div>
+          </dl>
+        </div>
+      </div>
+    </div>
+    <div class="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+      <div class="sm:text-center lg:text-left">
+        <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+          <span class="block xl:inline">Data to enrich your</span>
+          <span class="block text-indigo-600 xl:inline">online business</span>
+        </h1>
+        <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+          Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
+        </p>
+        <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+          <div class="rounded-md shadow">
+            <a href="#" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">
+              Get started
+            </a>
+          </div>
+          <div class="mt-3 sm:mt-0 sm:ml-3">
+            <a href="#" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10">
+              Live demo
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
