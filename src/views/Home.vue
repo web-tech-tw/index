73e9c7d2262cb6480c287fbd79a2eb05c331b656
SuperSonic <supersonic@livemail.tw>
2021-12-18 17:40:12 +0800

    Update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,15 @@
 <template>
   <div>
-    <div class="bg-gray-50 py-10">
-      <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
+    <div class="bg-gray-50 py-10 lg:py-16">
+      <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 lg:flex lg:items-center lg:justify-between">
         <h2 class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
           <span class="block mb-3">Taiwan Web Technology Promotion Organization</span>
           <span class="block text-indigo-600">臺灣網際網路技術推廣組織</span>
         </h2>
-        <p class="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
-          一個致力於網際網路發展的組織。
-        </p>
       </div>
+      <p class="text-xl text-gray-500 max-w-7xl mx-auto px-4 py-3 sm:px-6 lg:px-8 lg:flex lg:items-center lg:justify-between">
+        一個致力於網際網路發展的組織。
+      </p>
     </div>
     <div class="py-12 bg-white">
       <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
