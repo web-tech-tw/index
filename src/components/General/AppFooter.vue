f7231e511021573c6e426ca69e48dded9486e436
SuperSonic <supersonic@livemail.tw>
2021-12-18 17:34:03 +0800

    Update

--- src/components/General/AppFooter.vue
+++ src/components/General/AppFooter.vue
@@ -4,67 +4,36 @@
       <div class="flex">
         <div class="flex-none w-1/2 space-y-10 sm:space-y-8 lg:flex lg:space-y-0">
           <div class="lg:flex-none lg:w-1/2">
-            <h2 class="font-semibold text-gray-900">Getting Started</h2>
+            <h2 class="font-semibold text-gray-900">組織資訊</h2>
             <ul class="mt-3 space-y-2">
-              <li><a class="hover:text-gray-900" href="/docs/installation">Installation</a></li>
-              <li><a class="hover:text-gray-900" href="/docs/editor-setup">Editor Setup</a></li>
-              <li><a class="hover:text-gray-900" href="/docs/using-with-preprocessors">Using with
-                Preprocessors</a></li>
-              <li><a class="hover:text-gray-900" href="/docs/optimizing-for-production">Optimizing for
-                Production</a></li>
-              <li><a class="hover:text-gray-900" href="/docs/browser-support">Browser Support</a></li>
-              <li><a class="hover:text-gray-900" href="/docs/upgrade-guide">Upgrade Guide</a></li>
+              <li><a class="hover:text-gray-900" href="#">尚未提供</a></li>
             </ul>
           </div>
           <div class="lg:flex-none lg:w-1/2">
-            <h2 class="font-semibold text-gray-900">Core Concepts</h2>
+            <h2 class="font-semibold text-gray-900">學習資源</h2>
             <ul class="mt-3 space-y-2">
-              <li><a class="hover:text-gray-900" href="/docs/utility-first">Utility-First
-                Fundamentals</a></li>
-              <li><a class="hover:text-gray-900" href="/docs/hover-focus-and-other-states">Handling
-                Hover, Focus, and Other States</a></li>
-              <li><a class="hover:text-gray-900" href="/docs/responsive-design">Responsive Design</a>
-              </li>
-              <li><a class="hover:text-gray-900" href="/docs/dark-mode">Dark Mode</a></li>
-              <li><a class="hover:text-gray-900" href="/docs/reusing-styles">Reusing Styles</a></li>
-              <li><a class="hover:text-gray-900" href="/docs/adding-custom-styles">Adding Custom
-                Styles</a></li>
-              <li><a class="hover:text-gray-900" href="/docs/functions-and-directives">Functions &amp;
-                Directives</a></li>
+              <li><a class="hover:text-gray-900" href="#">尚未提供</a></li>
             </ul>
           </div>
         </div>
         <div class="flex-none w-1/2 space-y-10 sm:space-y-8 lg:flex lg:space-y-0">
           <div class="lg:flex-none lg:w-1/2">
-            <h2 class="font-semibold text-gray-900">Customization</h2>
+            <h2 class="font-semibold text-gray-900">就業資源</h2>
             <ul class="mt-3 space-y-2">
-              <li><a class="hover:text-gray-900" href="/docs/configuration">Configuration</a></li>
-              <li><a class="hover:text-gray-900" href="/docs/content-configuration">Content
-                Configuration</a></li>
-              <li><a class="hover:text-gray-900" href="/docs/theme">Theme Configuration</a></li>
-              <li><a class="hover:text-gray-900" href="/docs/screens">Customizing Screens</a></li>
-              <li><a class="hover:text-gray-900" href="/docs/customizing-colors">Customizing
-                Colors</a></li>
-              <li><a class="hover:text-gray-900" href="/docs/customizing-spacing">Customizing
-                Spacing</a></li>
-              <li><a class="hover:text-gray-900" href="/docs/plugins">Plugins</a></li>
-              <li><a class="hover:text-gray-900" href="/docs/presets">Presets</a></li>
+              <li><a class="hover:text-gray-900" href="#">尚未提供</a></li>
             </ul>
           </div>
           <div class="lg:flex-none lg:w-1/2">
-            <h2 class="font-semibold text-gray-900">Community</h2>
+            <h2 class="font-semibold text-gray-900">社交媒體</h2>
             <ul class="mt-3 space-y-2">
-              <li><a class="hover:text-gray-900"
-                     href="https://github.com/tailwindlabs/tailwindcss">GitHub</a></li>
-              <li><a class="hover:text-gray-900" href="/discord">Discord</a></li>
-              <li><a class="hover:text-gray-900" href="https://twitter.com/tailwindcss">Twitter</a>
-              </li>
-              <li><a class="hover:text-gray-900"
-                     href="https://www.youtube.com/tailwindlabs">YouTube</a></li>
+              <li><a class="hover:text-gray-900" href="#">尚未提供</a></li>
             </ul>
           </div>
         </div>
       </div>
+    </div>
+    <div class="max-w-7xl mx-auto mt-11 divide-y divide-gray-200 px-4 sm:px-6 md:px-8">
+      &copy; 2021 Taiwan Web Technology Promotion Organization(Web Tech TW)
     </div>
   </footer>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,111 +1,130 @@
 <template>
   <div>
-    <div class="bg-gray-50">
+    <div class="bg-gray-50 py-10">
       <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
         <h2 class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
           <span class="block mb-3">Taiwan Web Technology Promotion Organization</span>
           <span class="block text-indigo-600">臺灣網際網路技術推廣組織</span>
         </h2>
+        <p class="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+          一個致力於網際網路發展的組織。
+        </p>
       </div>
     </div>
     <div class="py-12 bg-white">
       <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="lg:text-center">
-          <h2 class="text-base text-indigo-600 font-semibold tracking-wide uppercase">Transactions</h2>
+          <h2 class="text-base text-indigo-600 font-semibold tracking-wide uppercase">我們的使命</h2>
           <p class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-            A better way to send money
+            讓世界看見台灣的資訊力
           </p>
           <p class="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
-            Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam voluptatum cupiditate veritatis in accusamus quisquam.
+            提供多樣化資訊產業資源，普及大眾資訊素養及資訊能力
           </p>
         </div>
-
         <div class="mt-10">
           <dl class="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
             <div class="relative">
               <dt>
                 <div class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-                  <!-- Heroicon name: outline/globe-alt -->
                   <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
                   </svg>
                 </div>
-                <p class="ml-16 text-lg leading-6 font-medium text-gray-900">Competitive exchange rates</p>
+                <p class="ml-16 text-lg leading-6 font-medium text-gray-900">推廣JavaScript</p>
               </dt>
               <dd class="mt-2 ml-16 text-base text-gray-500">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
+                Unavailable
               </dd>
             </div>
-
             <div class="relative">
               <dt>
                 <div class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-                  <!-- Heroicon name: outline/scale -->
-                  <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3" />
+                  <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
                   </svg>
                 </div>
-                <p class="ml-16 text-lg leading-6 font-medium text-gray-900">No hidden fees</p>
+                <p class="ml-16 text-lg leading-6 font-medium text-gray-900">豐富的學習資源</p>
               </dt>
               <dd class="mt-2 ml-16 text-base text-gray-500">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
+                Unavailable
               </dd>
             </div>
-
             <div class="relative">
               <dt>
                 <div class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-                  <!-- Heroicon name: outline/lightning-bolt -->
-                  <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z" />
-                  </svg>
-                </div>
-                <p class="ml-16 text-lg leading-6 font-medium text-gray-900">Transfers are instant</p>
-              </dt>
-              <dd class="mt-2 ml-16 text-base text-gray-500">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
-              </dd>
-            </div>
-
-            <div class="relative">
-              <dt>
-                <div class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-                  <!-- Heroicon name: outline/annotation -->
                   <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
                   </svg>
                 </div>
-                <p class="ml-16 text-lg leading-6 font-medium text-gray-900">Mobile notifications</p>
+                <p class="ml-16 text-lg leading-6 font-medium text-gray-900">提供社交媒體交流平台</p>
               </dt>
               <dd class="mt-2 ml-16 text-base text-gray-500">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
+                Unavailable
+              </dd>
+            </div>
+            <div class="relative">
+              <dt>
+                <div class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                  </svg>
+                </div>
+                <p class="ml-16 text-lg leading-6 font-medium text-gray-900">一切資源全部免費</p>
+              </dt>
+              <dd class="mt-2 ml-16 text-base text-gray-500">
+                Unavailable
               </dd>
             </div>
           </dl>
         </div>
       </div>
     </div>
-    <div class="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+    <div class="mt-10 mx-auto py-10 max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
       <div class="sm:text-center lg:text-left">
         <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-          <span class="block xl:inline">Data to enrich your</span>
-          <span class="block text-indigo-600 xl:inline">online business</span>
+          <span class="block xl:inline">我們的動力由</span>
+          <span class="block text-indigo-600 xl:inline">開放原始碼社群</span>
+          <span class="block xl:inline">所驅動</span>
         </h1>
         <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-          Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
+          臺灣網際網路技術推廣組織起源於開放原始碼社群，運用開放原始碼社群的技術，致力於提升資訊素養及網路技術推廣。
         </p>
         <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
           <div class="rounded-md shadow">
             <a href="#" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">
-              Get started
+              GitHub
             </a>
           </div>
           <div class="mt-3 sm:mt-0 sm:ml-3">
             <a href="#" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10">
-              Live demo
+              Discord
             </a>
           </div>
         </div>
+      </div>
+    </div>
+    <div class="mt-10 mx-auto py-10 max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+      <div class="sm:text-center lg:text-right">
+        <h1 class="text-4xl tracking-tight font-extrabold text-gray-900">
+          <span class="block xl:inline">我們被這些</span>
+          <span class="block text-indigo-600 xl:inline">贊助方</span>
+          <span class="block xl:inline">所支持</span>
+        </h1>
+        <p class="mt-3 text-base text-gray-500">
+          謝謝你們，因為有你們，我們才能更好
+        </p>
+      </div>
+    </div>
+    <div class="mt-10 mx-auto py-10 max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+      <div class="sm:text-center lg:text-left">
+        <h1 class="text-4xl tracking-tight font-extrabold text-gray-900">
+          <span class="block xl:inline">想加入我們嗎？</span>
+          <span class="block text-indigo-600 xl:inline">我們需要你</span>
+        </h1>
+        <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+          我們正在為大眾提供一個免費且開放的網際網路技術推廣平台，讓大眾可以透過網路，了解網際網路技術，嘗試提升大眾資訊素養，並參與網路技術推廣的討論與分享。
+        </p>
       </div>
     </div>
   </div>
