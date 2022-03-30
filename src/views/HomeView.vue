f3aaf96dd8a8406bfb093e49ee260765adc987f2
SuperSonic <supersonic@livemail.tw>
2022-03-31 04:13:45 +0800

    Update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,12 +3,12 @@
     <div class="bg-gray-50 py-10 lg:py-16">
       <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 lg:flex lg:items-center lg:justify-between">
         <h2 class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-          <span class="block mb-3">Taiwan Web Technology Promotion Organization</span>
-          <span class="block text-indigo-600">臺灣網際網路技術推廣組織</span>
+          <span class="block mb-3">{{ first_block.title }}</span>
+          <span class="block text-indigo-600">{{ first_block.subtitle }}</span>
         </h2>
       </div>
       <p class="text-xl text-gray-500 max-w-7xl mx-auto px-4 py-3 sm:px-6 lg:px-8 lg:flex lg:items-center lg:justify-between">
-        一個致力於網際網路發展的組織。
+        {{ first_block.description }}
       </p>
     </div>
     <div class="py-12 bg-white">
@@ -146,6 +146,34 @@
 
 <script>
 export default {
-  name: 'Home'
+  name: 'Home',
+  data: () => ({
+    first_block: {
+      title: "",
+      subtitle: "",
+      description: ""
+    }
+  }),
+  methods: {
+    firstBlock() {
+      const text = "Hello world\n早安，世界。\n一個致力於網際網路發展的組織。臺灣網際網路技術推廣組織";
+      const content_fields = Object.keys(this.first_block)
+      let field_counter = 0
+      console.log(content_fields)
+      for (const i in text) {
+        if (text[i] === "\n") {
+          field_counter++;
+          continue;
+        }
+        const field_name = content_fields[field_counter];
+        setTimeout(() => {
+          this.first_block[field_name] += text[i]
+        }, i * 30)
+      }
+    }
+  },
+  mounted() {
+    this.firstBlock()
+  }
 }
 </script>
