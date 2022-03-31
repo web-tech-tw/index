92a0404f95310ebfddbe5eb512d1c3fdb485975c
SuperSonic <supersonic@livemail.tw>
2022-03-31 08:22:33 +0800

    Update

--- src/components/General/AppHeader.vue
+++ src/components/General/AppHeader.vue
@@ -3,14 +3,14 @@
     <div class="max-w-7xl mx-auto px-4 sm:px-6">
       <div class="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
         <div class="flex justify-start lg:w-0 lg:flex-1">
-          <a href="/">
+          <router-link to="/">
             <h1 class="flex-auto text-lg font-semibold text-gray-900 hidden sm:block">
               Taiwan Web Technology Promotion Organization
             </h1>
             <h1 class="flex-auto text-lg font-semibold text-gray-900 sm:hidden">
               Web Tech TW
             </h1>
-          </a>
+          </router-link>
         </div>
         <nav class="md:flex space-x-10 hidden">
           <div v-for="(item, index) in menu" :key="index">
