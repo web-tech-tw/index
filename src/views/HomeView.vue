b9e1d58602b833f898b471e4efde66d85c15e8ce
SuperSonic <supersonic@livemail.tw>
2022-05-16 14:13:37 +0800

    Update Discord link

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -97,7 +97,7 @@
             </a>
           </div>
           <div class="mt-3 sm:mt-0 sm:ml-3">
-            <a href="https://discord.gg/xpyuq342nX" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10">
+            <a href="https://discord.gg/nM7ub6Dz5J" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10">
               Discord
             </a>
           </div>
