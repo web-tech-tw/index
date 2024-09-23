fcf9347e92f63afcdcbfa0c593aec0ff647ea221
不知火 Shiranui <supersonic@livemail.tw>
2024-09-24 02:13:32 +0800

    feat(appfooter.vue): update links

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -6,18 +6,18 @@
           <div class="lg:flex-none lg:w-1/2">
             <h2 class="font-semibold text-gray-900">組織資訊</h2>
             <ul class="mt-3 space-y-2">
-              <li><a class="hover:text-gray-900" href="#">核心成員</a></li>
-              <li><a class="hover:text-gray-900" href="#">財務報表</a></li>
-              <li><a class="hover:text-gray-900" href="#">關於組織</a></li>
+              <li><router-link class="hover:text-gray-900" to="/core-team">核心成員</router-link></li>
+              <li><router-link class="hover:text-gray-900" to="/financial">財務報表</router-link></li>
+              <li><router-link class="hover:text-gray-900" to="/about">關於組織</router-link></li>
             </ul>
           </div>
           <div class="lg:flex-none lg:w-1/2">
             <h2 class="font-semibold text-gray-900">產品列表</h2>
             <ul class="mt-3 space-y-2">
-              <li><a class="hover:text-gray-900" href="#">Swollen</a></li>
-              <li><a class="hover:text-gray-900" href="#">Sara 無密碼式身分認證系統</a></li>
-              <li><a class="hover:text-gray-900" href="#">meow 微型部落格社交平台</a></li>
-              <li><a class="hover:text-gray-900" href="#">更多產品</a></li>
+              <li><a class="hover:text-gray-900" href="https://github.com/web-tech-tw/swollen">Swollen</a></li>
+              <li><a class="hover:text-gray-900" href="https://github.com/web-tech-tw/sara">Sara 無密碼式身分認證系統</a></li>
+              <li><a class="hover:text-gray-900" href="https://github.com/web-tech-tw/meow">meow 微型部落格社交平台</a></li>
+              <li><a class="hover:text-gray-900" href="https://github.com/web-tech-tw">更多產品</a></li>
             </ul>
           </div>
         </div>
@@ -25,18 +25,18 @@
           <div class="lg:flex-none lg:w-1/2">
             <h2 class="font-semibold text-gray-900">就業資源</h2>
             <ul class="mt-3 space-y-2">
-              <li><a class="hover:text-gray-900" href="#">徵才平台</a></li>
-              <li><a class="hover:text-gray-900" href="#">求職平台</a></li>
-              <li><a class="hover:text-gray-900" href="#">職場討論</a></li>
+              <li><a class="hover:text-gray-900" href="/mejob/#/hiring">徵才平台</a></li>
+              <li><a class="hover:text-gray-900" href="/mejob/#/jobs">求職平台</a></li>
+              <li><a class="hover:text-gray-900" href="/mejob/#/chat">職場討論</a></li>
             </ul>
           </div>
           <div class="lg:flex-none lg:w-1/2">
             <h2 class="font-semibold text-gray-900">學習資源</h2>
             <ul class="mt-3 space-y-2">
-              <li><a class="hover:text-gray-900" target="_blank" href="#">為什麼我要更新瀏覽器呢？</a></li>
-              <li><a class="hover:text-gray-900" target="_blank" href="#">網際網路基礎觀念</a></li>
-              <li><a class="hover:text-gray-900" target="_blank" href="https://ncurl.xyz/s/b0AIBYTnR">JavaScript 基礎教學（MDN）</a></li>
-              <li><a class="hover:text-gray-900" target="_blank" href="#">網際網路後端伺服器開發基礎（Express.js）</a></li>
+              <li><a class="hover:text-gray-900" target="_blank" href="/wiki/#/browser">為什麼我要更新瀏覽器呢？</a></li>
+              <li><a class="hover:text-gray-900" target="_blank" href="/wiki/#/internet">網際網路基礎觀念</a></li>
+              <li><a class="hover:text-gray-900" target="_blank" href="/wiki/#/javascript">JavaScript 基礎教學（MDN）</a></li>
+              <li><a class="hover:text-gray-900" target="_blank" href="/wiki/#/express.js">網際網路後端伺服器開發基礎（Express.js）</a></li>
             </ul>
           </div>
         </div>
@@ -47,7 +47,7 @@
       <router-link class="mr-3" to="/privacy">隱私權政策</router-link>
     </div>
     <div class="max-w-7xl mx-auto mt-5 divide-y divide-gray-200 px-4 sm:px-6 md:px-8">
-      &copy; 2022 臺灣網際網路技術推廣組織 Taiwan Web Technology Promotion Organization (Web Tech TW)
+      &copy; 2024 臺灣網際網路技術推廣組織 Taiwan Web Technology Promotion Organization (Web Tech TW)
     </div>
   </footer>
 </template>
