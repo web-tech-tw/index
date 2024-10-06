24daff15debe56ce287f37fafe14d451ad02c609
不知火 Shiranui <supersonic@livemail.tw>
2024-10-06 22:42:04 +0800

    fix(homeview.vue): update ai community link

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -135,7 +135,7 @@
         </div>
         <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-end">
           <div class="rounded-md shadow">
-            <a href="https://ai-tech-tw.github.io"
+            <a href="https://ai.web-tech.tw"
               class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">
               人工智慧專屬社群
             </a>
